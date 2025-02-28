48d5fc377bf2b8220825587f52eb356deae0e69f
eliaskoziol <ekoziol@ciandt.com>
2025-02-28 11:18:22 -0300

    Adicionado animação de transição

--- front-new/src/app/dashboard/components/dashboard-home/dashboard-home.component.ts
+++ front-new/src/app/dashboard/components/dashboard-home/dashboard-home.component.ts
@@ -16,13 +16,32 @@ import { UtilService } from '../../../core/services/util.service';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { FormsModule } from '@angular/forms';
 import { TeamInterface } from '../../../core/interfaces/team.interface';
+import { animate, keyframes, query, stagger, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-dashboard-home',
   imports: [ButtonModule, CardModule, FieldsetModule, PanelModule, FluidModule, DataViewModule, DasboardCardComponent, ScrollerModule, AvatarModule, AvatarGroupModule, RadioButtonModule, FormsModule],
   templateUrl: './dashboard-home.component.html',
   styleUrl: './dashboard-home.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  animations: [
+    trigger('animation', [
+      transition('* <=> *', [
+        query(':enter', [
+          style({opacity: 0, transform: 'scale(0.7)'}),
+          stagger(400, [
+            animate('1800ms ease-in', style({opacity: 1, transform: 'scale(1)'}))
+          ])
+        ], { optional: true }),
+        query(':leave', [
+          style({opacity: 1, transform: 'scale(1)'}),
+          stagger(-200, [
+            animate('800ms ease-in', style({opacity: 0, transform: 'scale(0.7)'}))
+          ])
+        ], { optional: true })
+      ])
+    ])
+  ]
 })
 export class DashboardHomeComponent implements OnInit {
 
@@ -42,16 +61,16 @@ export class DashboardHomeComponent implements OnInit {
     this.peopleService.listAll().pipe(map(item => this.vision === 'engagement' ? this.utilService.transformPeopleToEngament(item) : this.utilService.transformPeopleToTeam(item))).subscribe((res: any) => {
       if (this.vision === 'team') {
         this.teams = res;
-        // interval(5000).pipe(takeWhile(() => this.vision === 'team')).subscribe(() => {
-        //   this.teams.push(<TeamInterface>this.teams.shift());
-        // });
+        interval(30000).pipe(takeWhile(() => this.vision === 'team')).subscribe(() => {
+          this.teams.push(<TeamInterface>this.teams.shift());
+        });
       }
 
       if (this.vision === 'engagement') {
         this.engagement = res;
-        // interval(5000).pipe(takeWhile(() => this.vision === 'engagement')).subscribe(() => {
-        //   this.engagement.push(<EngagementInterface>this.engagement.shift());
-        // });
+        interval(30000).pipe(takeWhile(() => this.vision === 'engagement')).subscribe(() => {
+          this.engagement.push(<EngagementInterface>this.engagement.shift());
+        });
       }
     });
   }
