69984681e2f7e21a4039106f7590ab081ec69416
eliaskoziol <ekoziol@ciandt.com>
2025-03-10 09:28:50 -0300

    Melhorias de interface

--- front-new/src/environments/environment.development.ts
+++ front-new/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
+  sheetsId: "1_9pROKA64tnAuOUQaDYR3u86Jc3Dk8om6WsCsX_rQls",
+  production: true,
+  googleClient: '146811529689-ruu06vc9k02cj29o8pp10g4i8724rc31.apps.googleusercontent.com',
+  apiKey: "AIzaSyAv5ot_8OdQAw4dz8FbS49UQM9XI9_LWdw",
   url: 'http://localhost:3000',
-  sheetsId: "",
-  production: false,
-  googleClient: "",
-  apiKey: "",
 };
